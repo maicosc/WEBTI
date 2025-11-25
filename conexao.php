6634b25b2e42be12aa319fe74b421f0a54d2b64f
Your Name <you@example.com>
2025-11-25 08:48:13 -0300

    Salvando update e delete

--- conexao.php
+++ conexao.php
@@ -19,4 +19,19 @@
         return $stmt->execute();
     }
     cadastraUsuario("Alianto Melrich", "meanddevil@evilmail.dark", "@vitoriadeBelphg0r");
+    function delete_usuario ($id){
+        $con = connecta_bd();
+        $stmt = $con -> prepare("DELETE FROM usuarios WHERE id = :id");
+        $stmt -> binParam(':id', $id);
+        return $stmt -> execute();
+    }
+    function update_usuario ($id, $nome, $login, $senha){
+        $con = connecta_bd();
+        $stmt = $con -> prepare("UPDATE usuarios SET nome = :nome, login = :login, senha = :senha WHERE id = :id");
+        $stmt -> binParam(':id', $id);
+        $stmt -> binParam(':login', $login);
+        $stmt -> binParam(':nome', $nome);
+        $stmt -> binParam(':senha', $senha);
+        return $stmt -> execute();
+    }
 ?>
