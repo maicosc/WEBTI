254875ed7eb6ce175243bbdde1604a6dea015d09
Maico S. Corrêa <maico.sc@aluno.ifsc.edu.br>
2025-12-07 21:33:08 -0300

    Atividade 3

---
+++ listaphp/atividade2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>R: O PHP não é uma linguagem do navegador como o JavaScript, ele atua na parte do servidor e não no navegador.</p>
+</body>
+</html>
---
+++ listaphp/atividade3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 3</title>
+</head>
+<body>
+    <?php
+    function hello(){
+        echo "Hello";
+    }
+    ?>
+    <p><?php hello()?></p>
+</body>
+</html>
